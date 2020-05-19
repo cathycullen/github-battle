38ed2ae78aa840a50c6254573b6f3d11e4d62a77
Cathy Cullen <cathy@softwareoptionscom>
2020-05-19 07:55:16 -0500

    format results

--- app/components/popular (C02SR1C8G8WL's conflicted copy 2020-05-18).js
+++ app/components/popular (C02SR1C8G8WL's conflicted copy 2020-05-18).js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos } from '../utils/api'
-import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle} from 'react-icons/fa'
 
 // extract render() nav to it's own functional component
 
@@ -29,48 +28,17 @@ LanguagesNav.propTypes = {
 }
 
 function ReposGrid({repos}) {
+  debugger
   return (
     <ul className='grid space-around'>
-      {repos.map( (repo, index) => {
-        const { name, html_url, owner, stargazers_count, forks, open_issues } = repo
-        const {login, avatar_url} = owner
-
-        return (
-          <li key = {html_url} className = 'repo bg-light '>
-            <h4 className= 'header-lg center-text'>
-              #{index+1}
-            </h4>
-            <img
-              className='avatar'
-              src={avatar_url}
-              alt={`Avatar for ${login}`}>
-            </img>
-            <h2 className='center-text'>
-              <a className='link' href={html_url}>{login}</a>
-            </h2>
-            <ul className='card-list'>
-              <li>
-                <FaUser color='rgb(255,191,116)' size={22}/>
-                <a href={`https://github.com/${login}`} >
-                  {login}
-                </a>
-              </li>
-              <li>
-                <FaStar color='rgb(255,215,0)' size={22}/>
-                {stargazers_count.toLocaleString()} stars
-              </li>
-              <li>
-                <FaCodeBranch color='rgb(129,195,245)' size={22}/>
-                {forks.toLocaleString()} forks
-              </li>
-              <li>
-                <FaExclamationTriangle color='rgb(241,138,147)' size={22}/>
-                {open_issues.toLocaleString()} open
-              </li>
-            </ul>
-          </li>
-        )
-      })}
+    {repos.map( (repo, index) => {
+      const { name, owner, stargazers_count, forks, open_issues } = repo
+      const {login, avatar_url} = owner
+    return (
+      <li key = {name}>{name} className = 'repo bg-light'</li>}
+      <h4 className= 'header-lg'>{name}</h4.
+    )
+  })}nam
     </ul>
   )
 }
