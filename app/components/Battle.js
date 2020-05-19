d7049560b38f45e81d752c713c3df7a2fcdf475f
Cathy Cullen <cathy@softwareoptionscom>
2020-05-19 11:32:47 -0500

    fix spaces

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -11,15 +11,15 @@ function Instructions() {
       <ol className='container-sm grid center-text battle-instructions'>
         <li>
           <h3 className='header-sm'>Enter two github users</h3>
-          <FaUserFriends className='bg-light' color='rgb(255,191,116)' size={140}/>
+          <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140}/>
         </li>
         <li>
           <h3 className='header-sm'>Battle</h3>
-          <FaFighterJet className='bg-light'  color='#727272' size={140}/>
+          <FaFighterJet className='bg-light' color='#727272' size={140}/>
         </li>
         <li>
           <h3 className='header-sm'>See the winner</h3>
-          <FaTrophy className='bg-light'  color='rgb(255,215,0)' size={140}/>
+          <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140}/>
         </li>
       </ol>
     </div>
