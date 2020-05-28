a91348100afbdd4335b0849623e3e4e117c85b03
Cathy Cullen <cathy@softwareoptionscom>
2020-05-28 14:54:22 -0500

    handle reset and show user if there else show input

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 
 
@@ -88,7 +88,24 @@ PlayerInput.propTypes = {
 function PlayerPreview({username, onReset, label}) {
   return (
     <div className = 'column player'>
-    {username}
+      <h3 className= 'player-label'> {label} </h3>
+      <div className = 'row bg-light'>
+        <div className = 'player-info'>
+        <img
+          className='avatar-small'
+          src={`https://github.com/${username}.png?size=200`}
+          alt={`Avatar for ${username}`}
+        />
+        <a
+          href={`https://github.com/${username}`}
+          className='link'>
+            {username}
+        </a>
+        <button className='btn-clear flex-center' onClick={onReset}>
+          <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+        </button>
+        </div>
+      </div>
     </div>
   )
 }
@@ -108,6 +125,7 @@ export default class Battle extends React.Component {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
   handleSubmit(id, player) {
     console.log(`handleSubmit called id: `, id )
@@ -117,6 +135,15 @@ export default class Battle extends React.Component {
     })
     console.log(this.state)
   }
+
+  handleReset(id) {
+    console.log("handleReset")
+
+    this.setState({
+      [id]: null
+    })
+  }
+
   render() {
     const { playerOne, playerTwo} = this.state
     return (
@@ -126,18 +153,29 @@ export default class Battle extends React.Component {
         <div className = 'players-container'>
           <h1 className = 'center-text header-lg'>Players</h1>
           <div className = 'row space-around'>
-            {playerOne === null && (
-              <PlayerInput
-                label = 'Player One'
-                onSubmit = { (player) => this.handleSubmit('playerOne', player)}
+            {playerOne === null
+              ? <PlayerInput
+                  label = 'Player One'
+                  onSubmit = { (player) => this.handleSubmit('playerOne', player)}
               />
-            )}
-            {playerTwo === null && (
-              <PlayerInput
-                label = 'Player Two'
-                onSubmit = { (player) => this.handleSubmit('playerTwo', player)}
+              : <PlayerPreview
+                username = {playerOne}
+                label='Player One'
+                onReset = { () => this.handleReset('playerOne')}
+                />
+            }
+
+            {playerTwo === null
+              ? <PlayerInput
+                  label = 'Player Two'
+                  onSubmit = { (player) => this.handleSubmit('playerTwo', player)}
               />
-            )}
+              : <PlayerPreview
+                username = {playerTwo}
+                label='Player Two'
+                onReset = { () => this.handleReset('playerTwo')}
+                />
+            }
           </div>
         </div>
       </React.Fragment>
