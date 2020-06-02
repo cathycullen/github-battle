2886e1640c9afe55297d9eb714e91efabe479031
Cathy Cullen <cathy@softwareoptionscom>
2020-06-02 10:14:52 -0500

    add state to results

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,20 +2,41 @@ import React from 'react'
 import { battle } from '../utils/api'
 
 export default class Results extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      winner: null,
+      loser: null,
+      loading: true,
+      error: false
+    }
+  }
   componentDidMount() {
     const { playerOne, playerTwo } = this.props
 
     battle([ playerOne, playerTwo ])
       .then((players) => {
         console.log('componentDidMount().then players: ', players)
-      })
+        this.setState({
+          winner: players[0],
+          loser: players[1],
+          loading: false,
+          error: null
+        })
+      }).catch(({message}) => {
+        this.setState({
+          error: message,
+          loading: false,
+        })
+      } )
   }
 
-  render() {]
+  render() {
     return (
       <div>
         Results
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     )
   }
